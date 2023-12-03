d893daf09502fc781e802435e75284f060ae82df
DAO Hamadou <daohamadou@gmail.com>
2023-12-03 14:39:24 +0000

    '-'

--- frontend/src/app/variable-service.service.ts
+++ frontend/src/app/variable-service.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class VariableServiceService {
-  adresse = location.origin + '/backend/public/index.php';
+  adresse = location.origin + '/backend/public/index.php/api';
   url: string = location.origin.includes('activite.elite-afrique.com')? this.adresse: 'http://127.0.0.1:8000/api';
   constructor() { 
     // console.log(this.url)
